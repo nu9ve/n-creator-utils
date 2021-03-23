06ad85fce7c0b1484a64f37ba40697f28cabb086
nu9ve <nu9ve@nu9ves-MacBook-Pro.local>
2021-03-23 08:11:24 -0700

    refactor file manager, add date updater for malconfigured gopro

--- manager/video.py
+++ manager/video.py
@@ -4,9 +4,11 @@ import subprocess
 import contextlib
 import shutil
 import logging
-from os.path import join as join_path
+from os.path import join as join_path, exists as path_exists
 
-from utils.constants import LOGGER_NAME, MONTH_STRING_NUMBERS, SD_CAM_NAME
+from utils.constants import (LOGGER_NAME, MONTH_STRING_NUMBERS, MONTH_NUMBER_DAYS,
+  SD_CAM_NAME, FINAL_DRON_DIR, FINAL_4K_DIR, FINAL_VIDEOS_DIR, FINAL_IMAGES_DIR, 
+  FINAL_SCREENS_DIR, MONTH_NUMBER_STRINGS)
 
 
 logger = logging.getLogger(LOGGER_NAME)
@@ -53,14 +55,19 @@ def get_video_file_duration(file_path):
     file_duration_string += '{}'.format(ss)
   return duration, file_duration_string
 
+def get_file_modified_time(file_path):
+  # ['Wed', 'Apr', '20', '06:13:20', '2016']
+  # ['06', '13', '20']
+  time_string = time.ctime(os.path.getmtime(file_path))
+  ts = list(filter(None, time_string.split(' ')))
+  tds = ts[3].split(':')
+  return ts, tds
 
 def get_video_file_description(folder, cam_name, video_file):
   video_data = dict()
   video_file_path = join_path(folder, video_file)
-  time_string = time.ctime(os.path.getmtime(video_file_path))
-  ts = list(filter(None, time_string.split(' ')))
   duration, file_duration_string = get_video_file_duration(video_file_path)
-  tds = ts[3].split(':')
+  ts, tds = get_file_modified_time(video_file_path)
   file_date_string = '{}{}{}-{}{}{}'.format(ts[4][-2:],ts[1].upper(),ts[2],tds[0],tds[1],tds[2])
   new_name = '{}_{}_{}'.format(file_date_string, cam_name.upper(), file_duration_string)
   video_data['duration'] = duration
@@ -69,20 +76,85 @@ def get_video_file_description(folder, cam_name, video_file):
   video_data['year'] = ts[4]
   return new_name, video_data
 
+def get_file_description(file_path, video_file, image_file, audio_file):
+  file_data = dict()
+  ts, tds = get_file_modified_time(file_path)
+  if video_file:
+    duration, file_duration_string = get_video_file_duration(file_path)
+    file_data['duration'] = duration
+    file_data['duration_string'] = file_duration_string
 
-def get_image_file_description(folder, cam_name, video_file):
-  video_data = dict()
-  video_file_path = join_path(folder, video_file)
-  time_string = time.ctime(os.path.getmtime(video_file_path))
-  ts = list(filter(None, time_string.split(' ')))
-  tds = ts[3].split(':')
-  file_date_string = '{}{}{}-{}{}{}'.format(ts[4][-2:],ts[1].upper(),ts[2],tds[0],tds[1],tds[2])
-  new_name = '{}_{}'.format(file_date_string, cam_name.upper())
-  video_data['day'] = ts[2]
-  video_data['month'] = MONTH_STRING_NUMBERS[ts[1]]
-  video_data['year'] = ts[4]
-  return new_name, video_data
+  file_data['date'] = ts
+  file_data['time'] = tds
+  return file_data
 
+def complete_time_string(ts):
+  time_string = str(ts)
+  if ts < 10:
+    time_string = '0' + str(ts)
+  return time_string
+
+def is_4k(file_path):
+  is4k = False
+  probe_cmd = [ 'ffprobe', '-v', 'error', '-select_streams', 'v:0', 
+          '-show_entries', 'stream=width,height,r_frame_rate', '-of', 'csv=s=x:p=0', file_path]
+  result = subprocess.run(probe_cmd, capture_output=True)
+  dims = str(result.stdout).split('x')
+  v_w = dims[0].split("'")[1]
+  v_h = dims[1].split("'")[0].replace('\\n','')
+  if int(v_w) > 2000 or int(v_h) > 2000:
+    is4k = True
+  return is4k
+
+def update_file_time(ts, tds):
+  # update for gopro no time setup
+
+  day_update = 13
+  month_update = 10
+  minute_update = 48
+  year_update = 4
+  update_day = False
+  update_month = False
+  update_year = False
+
+  new_year = int(ts[4]) + year_update
+  new_minute = int(tds[1]) + minute_update
+  new_month = int(MONTH_STRING_NUMBERS[ts[1]]) + month_update
+  new_day = int(ts[2]) + day_update
+
+  if new_minute > 59:
+    new_hour = int(tds[0]) + 1
+    new_minute = new_minute % 60
+    tds[1] = complete_time_string(new_minute)
+    if new_hour > 24:
+      new_hour = new_hour % 24
+      update_day = True
+    tds[0] = complete_time_string(new_hour)
+  if new_month > 12:
+    new_month = new_month % 12
+    update_year = True
+  month_string = complete_time_string(new_month)  
+
+  if update_day:
+    new_day = new_day + 1
+  if new_day > MONTH_NUMBER_DAYS[month_string]:
+    update_month = True
+    new_day = new_day % MONTH_NUMBER_DAYS[month_string]
+  if update_month:
+    new_month = new_month + 1
+    if new_month > 12:
+      new_month = new_month % 12
+      update_year = True
+    month_string = complete_time_string(new_month)  
+  if update_year:
+    new_year = new_year + 1
+  
+  ts[1] = MONTH_NUMBER_STRINGS[month_string]
+  ts[2] = str(new_day)
+  ts[3] = ':'.join(tds)
+  ts[4] = str(new_year)
+  
+  return ts, tds
 
 def store_video_files():
   # erikpyado save
@@ -92,111 +164,86 @@ def store_video_files():
   #   month (1-12)
   #     year(19)month(01-12)day(01-31)_micname_duration(mmm-ss)_
   #     191131-2355_H6LR_20-02_JP-TOKYO[optional(place,mood)].WAV
-  done_test = False
-  root_dir = '/Volumes/HDD4'
-  final_dron_dir = join_path(root_dir,'vdron')
-  final_4k_dir = join_path(root_dir,'v4k')
-  final_videos_dir = join_path(root_dir,'videos')
-  final_images_dir = join_path(root_dir,'images')
-  final_screens_dir = join_path(root_dir,'screens')
 
+  done_test = False
+
+  # get_external_storage
   if len(sys.argv) != 6:
     error = 'erikpyado videos src_dir cam jp tag'
     logger.error(error)
     return
+
   source_dir = sys.argv[2]
   cam_name = sys.argv[3]
   country_code = sys.argv[4]
   city_name = sys.argv[5]
+
   for r, ds, fs in os.walk(source_dir):
-    # for folder in ds:
-      # recording_folder = join_path(r, folder)
-      # for vr, vds, vfs in os.walk(recording_folder):
-    for video_file in fs:
-      if '.PNG' == video_file[-4:] or '.png' == video_file[-4:]:
-        new_file_name, file_data = get_image_file_description(source_dir,cam_name,video_file)
-        new_file_name += '_{}-{}[].png'.format(country_code.upper(), city_name.upper())
-        initial_path = join_path(source_dir, video_file)
-        if 'screenshot' in initial_path or 'Screenshot' in initial_path:
-          final_dir = join_path(final_screens_dir,file_data['year'],file_data['month'])
+    for media_file in fs:
+      move_file = False
+      delete_file = False
+      video_file = False
+      image_file = False
+      audio_file = False
+      source_path = join_path(source_dir, media_file)
+      media_file_parts = media_file.split('.')
+      file_extension = media_file_parts[len(media_file_parts)-1].lower()
+
+      if 'png' == file_extension or 'jpg' == file_extension or 'jpeg' == file_extension:
+        image_file = True
+      if 'mov' == file_extension or 'mp4' == file_extension:
+        video_file = True
+      if 'wav' == file_extension:
+        audio_file = True
+      if 'lrv' == file_extension or 'thm' == file_extension:
+        delete_file = True
+
+      file_data = get_file_description(source_path, video_file, image_file, audio_file)
+      
+      if 'GOP' in media_file[:3] or 'GP' in media_file[:3] or 'G0' in media_file[:2]:
+        cam_name = 'GOPRO'
+        if int(file_data['date'][4]) < 2018:
+          file_data['date'], file_data['time'] = update_file_time(file_data['date'], file_data['time'])
+      
+      file_data['day'] = file_data['date'][2]
+      file_data['month'] = MONTH_STRING_NUMBERS[file_data['date'][1]]
+      file_data['year'] = file_data['date'][4]
+
+      file_date_string = '{}{}{}-{}{}{}'.format(file_data['date'][4][-2:],file_data['date'][1].upper(),file_data['date'][2],file_data['time'][0],file_data['time'][1],file_data['time'][2])
+      new_file_name = '{}_{}'.format(file_date_string, cam_name.upper())
+
+      if image_file:
+        new_file_name += '_{}-{}[].{}'.format(country_code.upper(), city_name.upper(), file_extension)
+        if 'screenshot' in source_path or 'Screenshot' in source_path:
+          final_dir = join_path(FINAL_SCREENS_DIR,file_data['year'],file_data['month'])
         else:
-          final_dir = join_path(final_images_dir,file_data['year'],file_data['month'])
+          final_dir = join_path(FINAL_IMAGES_DIR,file_data['year'],file_data['month'])
         if not os.path.exists(final_dir): 
           os.makedirs(final_dir)
         final_path = join_path(final_dir, new_file_name)
-        print(final_path)
-        
+        move_file = True
 
-        # shutil.move(initial_path, final_path)
-
-
-      if '.JPG' == video_file[-4:] or '.jpg' == video_file[-4:]:
-        if 'GOP' in video_file[:3] or 'GP' in video_file[:3] or 'G0' in video_file[:2]:
-          cam_name = 'GOPRO'
-        new_file_name, file_data = get_image_file_description(source_dir,cam_name,video_file)
-        new_file_name += '_{}-{}[].jpg'.format(country_code.upper(), city_name.upper())
-        initial_path = join_path(source_dir, video_file)
-        if 'screenshot' in initial_path or 'Screenshot' in initial_path:
-          final_dir = join_path(final_screens_dir,file_data['year'],file_data['month'])
-        else:
-          final_dir = join_path(final_images_dir,file_data['year'],file_data['month'])
-        if not os.path.exists(final_dir): 
-          os.makedirs(final_dir)
-        final_path = join_path(final_dir, new_file_name)
-        print(final_path)
-
-
-        shutil.move(initial_path, final_path)
-
-
-      if '.MP4' == video_file[-4:] or '.mp4' == video_file[-4:]:
-        if 'GOP' in video_file[:3] or 'GP' in video_file[:3]:
-          cam_name = 'GOPRO'
-        new_file_name, file_data = get_video_file_description(source_dir,cam_name,video_file)
-        new_file_name += '_{}-{}[].mp4'.format(country_code.upper(), city_name.upper())
-        # if file_data['duration'] > 600: #10 min
-        #   final_dir = join_path(long_recordings_dir,file_data['year'],file_data['month'])
-        # else:
-
-
-        initial_path = join_path(source_dir, video_file)
-        is4k = False
-        probe_cmd = [ 'ffprobe', '-v', 'error', '-select_streams', 'v:0', 
-          '-show_entries', 'stream=width,height,r_frame_rate', '-of', 'csv=s=x:p=0', initial_path]
-        # print(' '.join(probe_cmd))
-        result = subprocess.run(probe_cmd, capture_output=True)
-        dims = str(result.stdout).split('x')
-        v_w = dims[0].split("'")[1]
-        v_h = dims[1].split("'")[0].replace('\\n','')
-        if int(v_w) > 2000 or int(v_h) > 2000:
-          is4k = True
-
-        category_dir = final_videos_dir
+      if video_file:
+        new_file_name += '_{}'.format(file_data['duration_string'])
+        new_file_name += '_{}-{}[].{}'.format(country_code.upper(), city_name.upper(), file_extension)
+        is4k = is_4k(source_path)
+        category_dir = FINAL_VIDEOS_DIR
         if is4k:
-          category_dir = final_4k_dir
+          category_dir = FINAL_4K_DIR
         final_dir = join_path(category_dir,file_data['year'],file_data['month'])
         if not os.path.exists(final_dir): 
           os.makedirs(final_dir)
         final_path = join_path(final_dir, new_file_name)
-        print(final_path)
+        move_file = True
 
+      logger.info('source: {}'.format(source_path))
+      if move_file:
+        logger.info('moving to: {}\n'.format(final_path))
+        # shutil.move(source_path, final_path)
+      if delete_file:
+        logger.info('deleting: {}\n'.format(source_path))
+        # shutil.move(source_path, final_path)
+      if not delete_file and not move_file:
+        logger.info('no action: {}\n'.format(source_path))
 
-        shutil.move(initial_path, final_path)
-
-
-      if '.MOV' == video_file[-4:] or '.mov' == video_file[-4:]:
-        new_file_name, file_data = get_video_file_description(source_dir,cam_name,video_file)
-        new_file_name += '_{}-{}[].mov'.format(country_code.upper(), city_name.upper())
-        # if file_data['duration'] > 600: #10 min
-        #   final_dir = join_path(long_recordings_dir,file_data['year'],file_data['month'])
-        # else:
-        final_dir = join_path(final_videos_dir,file_data['year'],file_data['month'])
-        if not os.path.exists(final_dir): 
-          os.makedirs(final_dir)
-        final_path = join_path(final_dir, new_file_name)
-        initial_path = join_path(source_dir, video_file)
-        print(final_path)
-
-
-        # shutil.move(initial_path, final_path)
 
--- utils/constants.py
+++ utils/constants.py
@@ -1,10 +1,59 @@
+from os.path import join as join_path
+
+
 LOGGER_NAME = 'kiddie'
+
+ROOT_DIR = '/Volumes/HDD4'
+ROOT_DIR = '~/Movies'
+
+FINAL_DRON_DIR = join_path(ROOT_DIR,'vdron')
+FINAL_4K_DIR = join_path(ROOT_DIR,'v4k')
+FINAL_VIDEOS_DIR = join_path(ROOT_DIR,'videos')
+FINAL_IMAGES_DIR = join_path(ROOT_DIR,'images')
+FINAL_SCREENS_DIR = join_path(ROOT_DIR,'screens')
 
 SD_CAM_NAME = {
   'EOS_DIGITAL': '',
   'GO': '',
   'downloads': '',
   'DJI': ''
+}
+
+SD_RECORDER_NAME = {
+  'H6_SD': ''
+}
+
+# from sys import platform
+# if platform == "linux" or platform == "linux2":
+#     # linux
+# elif platform == "darwin":
+#     # OS X
+# elif platform == "win32":
+#     # Windows...
+
+# os.unmount(path)
+# os.system("drutil tray open")
+# linux
+# os.system("eject -t cdrom")
+
+# /audio
+#   /recordings
+#   /recordings_long
+#   /sounds
+#   /songs
+#
+# /images
+#   /camera
+#   /phone
+#   /screens
+#
+# /video
+#   /drone
+#   /4k
+#   /1080
+
+FINAL_DIRS = {
+  'DRONE': 'dron'
 }
 
 MONTH_STRING_NUMBERS = {
@@ -20,4 +69,36 @@ MONTH_STRING_NUMBERS = {
   'Oct': '10',
   'Nov': '11',
   'Dec': '12',
+}
+
+
+MONTH_NUMBER_STRINGS = {
+  '01': 'Jan',
+  '02': 'Feb',
+  '03': 'Mar',
+  '04': 'Apr',
+  '05': 'May',
+  '06': 'Jun',
+  '07': 'Jul',
+  '08': 'Aug',
+  '09': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec',
+}
+
+
+MONTH_NUMBER_DAYS = {
+  '01': 31,
+  '02': 28,
+  '03': 31,
+  '04': 30,
+  '05': 31,
+  '06': 30,
+  '07': 31,
+  '08': 31,
+  '09': 30,
+  '10': 31,
+  '11': 30,
+  '12': 31,
 }
